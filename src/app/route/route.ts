a9ab01c5d4dd2e6c563cf83dfe298249692133d6
Thiago Henrique de Godoy <thiagodoy@hotmail.com>
2017-02-14 20:33:44 -0200

    Implementação inicial edição do usuario

--- src/app/route/route.ts
+++ src/app/route/route.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { ListApiaryComponent } from '../list-apiary/list-apiary.component';
 import { EditApiaryComponent } from '../edit-apiary/edit-apiary.component';
+import { EditUserComponent } from '../edit-user/edit-user.component';
 
 
 export const route: Routes = [
     { path: '', component: ListApiaryComponent },
-    { path: 'edit', component: EditApiaryComponent}
+    { path: 'edit', component: EditApiaryComponent},
+    { path: 'user', component: EditUserComponent}
 
 ]    
