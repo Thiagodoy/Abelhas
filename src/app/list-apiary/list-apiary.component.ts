b2607c7dfda19574a390d93f4e76cacf18d7042d
Thiago Henrique de Godoy <thiagodoy@hotmail.com>
2017-02-22 07:11:21 -0300

    Configuração do Parse

--- src/app/list-apiary/list-apiary.component.ts
+++ src/app/list-apiary/list-apiary.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, ElementRef, ViewContainerRef, ViewChild } from '@angular/core';
 import { TableComponent } from '../table/table.component';
-import { ITdDataTableColumn, IPageChangeEvent, TdDataTableService, TdDataTableSortingOrder,TdDataTableComponent } from '@covalent/core';
+import { ITdDataTableColumn, IPageChangeEvent, TdDataTableService, TdDataTableSortingOrder, TdDataTableComponent } from '@covalent/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 
 import { DialogService } from '../service/dialog.service';
+import { ParseService } from '../service/parse.service';
+
+
+// TESTE PARSE
+import * as parse from 'parse';
 
 
 
@@ -17,59 +22,68 @@ export class ListApiaryComponent implements OnInit {
 
   // MOCK
   private data: any[] = [
-    { id:1,validado: 'Sim', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:2,validado: 'Sim', especie: 'Meliponinae', apicultor: 'Rodrigo', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:3,validado: 'Sim', especie: 'Meliponinae', apicultor: 'Marcos', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:4,validado: 'Sim', especie: 'Meliponinae', apicultor: 'Thiago', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:5,validado: 'Sim', especie: 'Meliponinae', apicultor: 'Diego', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:6,validado: 'Sim', especie: 'Apis Mellifera', apicultor: 'Delmas Mattos', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:7,validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Felipe Auguusto', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:8,validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Cristiano da Silva', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:9,validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Anderson Merchan', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:10,validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Iuian', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:11,validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Daise', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:12,validado: 'Não', especie: 'Meliponinae', apicultor: 'Bruno', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:13,validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:14,validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:15,validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:20,validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:16,validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:17,validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:18,validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
-    { id:19,validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 1, validado: 'Sim', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 2, validado: 'Sim', especie: 'Meliponinae', apicultor: 'Rodrigo', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 3, validado: 'Sim', especie: 'Meliponinae', apicultor: 'Marcos', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 4, validado: 'Sim', especie: 'Meliponinae', apicultor: 'Thiago', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 5, validado: 'Sim', especie: 'Meliponinae', apicultor: 'Diego', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 6, validado: 'Sim', especie: 'Apis Mellifera', apicultor: 'Delmas Mattos', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 7, validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Felipe Auguusto', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 8, validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Cristiano da Silva', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 9, validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Anderson Merchan', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 10, validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Iuian', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 11, validado: 'Não', especie: 'Apis Mellifera', apicultor: 'Daise', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 12, validado: 'Não', especie: 'Meliponinae', apicultor: 'Bruno', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 13, validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 14, validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 15, validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 20, validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 16, validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 17, validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 18, validado: 'Erro', especie: 'Apis Mellifera', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
+    { id: 19, validado: 'Erro', especie: 'Meliponinae', apicultor: 'Marcelo de Souza', status: 'Modificado pelo gestor', data: '12/12/2015', },
 
   ];
 
   columns: ITdDataTableColumn[] = [
-    { name: 'validado',  label: 'Validado' },
+    { name: 'validado', label: 'Validado' },
     { name: 'especie', label: 'Espécie' },
-    { name: 'apicultor', label: 'Apicultor'},
-    { name: 'status', label: 'Status'},
-    { name: 'data', label: 'Data'},
-    { name: 'acoes', label: 'Ações'},  ];
+    { name: 'apicultor', label: 'Apicultor' },
+    { name: 'status', label: 'Status' },
+    { name: 'data', label: 'Data' },
+    { name: 'acoes', label: 'Ações' },];
 
-  
 
-  
-  
+
+
+
   sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-  
+
   dateFormat: string = 'DD-MM-YYYY';
   font: string = 'Roboto,"Helvetica Neue",sans-serif';
 
-  constructor(private dialogService: DialogService, private viewContainerRef: ViewContainerRef, private _dataTableService: TdDataTableService) { 
+  constructor(private dialogService: DialogService,
+    private viewContainerRef: ViewContainerRef,
+    private _dataTableService: TdDataTableService,
+    private serviceParse:ParseService) {
 
   }
 
-  ngOnInit() {  
-   
-  }
+  ngOnInit() {
 
- 
-  itemSelected(event){
-   console.log(event);
   }
 
 
-  
+  itemSelected(event) {
+    console.log(event);
+  }
+
+  testeLogin() {
+    let user = new parse.User();
+    user.setUsername('thiago');
+    user.setPassword('thiago');
+  debugger;
+    this.serviceParse.login(user);
+
+  }
 }
--- src/app/service/parse.service.ts
+++ src/app/service/parse.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 import * as parse from 'parse';
+import { environment } from '../../environments/environment.prod'
 
 @Injectable()
 export class ParseService {
+ core = parse;
+  //showToolbar: EventEmitter<boolean> = new EventEmitter<boolean>()
+  //static loaderEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  //usuarioLogado: Login;  
+  
+  constructor() {
 
-  constructor() { }
+    this.core.initialize("myAppDebug");
+    this.core.serverURL = "http://localhost:1337/parse";
+    // Mapeia as classes que serão utilizadas no parse
+    //this.core.Object.registerSubclass("GameScore", GameScore);
+    //this.core.Object.registerSubclass("User", Login);
+
+  }
+
+  findAll<T extends parse.Object>(paramClass: { new (): T }): parse.Promise<T[]> {
+    let query = new this.core.Query(new paramClass());
+    return query.find();
+  }
+
+  login(user: Parse.User) {
+    user.logIn().then(res => {
+      debugger;
+      console.log(res);
+      //this.router.navigate(["home"]);
+     // this.showToolbar.emit(true);
+    });
+
+
+  }
 
 }
